2342ca1e43cf9b6241a8ec1ece327f26ee7b67e4
SauravDas90 <saurav.d336@gmail.com>
2016-08-05 00:17:48 +0530

    git-ignore and port changes
    port no changed
    gitignore added

--- springboot/src/main/java/net/element/bootapp/HelloController.java
+++ springboot/src/main/java/net/element/bootapp/HelloController.java
@@ -1,10 +1,15 @@
 package net.element.bootapp;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	
+	public static final String LOGGER=null;
 	
 	@RequestMapping("/web/boot")
 	public String restBootCall(){
@@ -19,5 +24,31 @@ public class HelloController {
 		System.out.println("Calling Android app");
 		return "Android call received successfully";
 	}
+	@RequestMapping(value="/web/login", method={RequestMethod.POST,RequestMethod.GET},consumes="application/x-www-form-urlencoded")
+	public String validateLogin(@RequestBody User user){
+		
+		if(user != null && (user.getUserName()!=null || !user.getUserName().isEmpty()))
+		{
+			if(user != null && (user.getPassword()!=null || !user.getPassword().isEmpty()))
+			{
+				if(user.getPassword().equalsIgnoreCase("Ger@123"))
+					return "Authentication SuccessFul \n";
+					
+			}
+		}
+		
+		return "Authentication Failed. Try again \n";
+	}
+	
+	@RequestMapping(value="/web/login1", method={RequestMethod.POST,RequestMethod.GET})
+	public String validateLoginGet(@RequestParam("name") String user){
+
+				if(user.equalsIgnoreCase("Ger@123"))
+					return "Authentication SuccessFul \n";
+				else
+		
+		return "Authentication Failed. Try again \n";
+	}
+	
 
 }
